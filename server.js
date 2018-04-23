35d9daf0bb2ddf354f2385d78d60c4fe41659fd9
Lucas <lucasalvarado23@gmail.com>
2018-04-23 10:09:09 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+ // app.use('/', apiRoutes, htmlRoutes);
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
